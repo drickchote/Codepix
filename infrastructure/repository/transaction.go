ed8fff2353e2092fe35ddb0270b83ae5311f6f7d
Marcos Adriano <mdrick19@gmail.com>
2021-02-04 02:33:21 -0300

    repository

---
+++ infrastructure/repository/transaction.go
@@ -0,0 +1,43 @@
+package repository
+
+import (
+	"fmt"
+
+	"github.com/drickchote/Codepix/domain/model"
+	"github.com/jinzhu/gorm"
+)
+
+// Register(transaction *Transaction) error
+// Save(transaction *Transaction) error
+// Find(id string) (*Transaction, error)
+
+type TransactionRepositoryDb struct {
+	Db *gorm.DB
+}
+
+func (r TransactionRepositoryDb) Register(transaction *model.Transaction) (*model.Transaction, error) {
+	err := r.Db.Create(transaction).Error
+	if err != nil {
+		return nil, err
+	}
+	return transaction, nil
+}
+
+func (r TransactionRepositoryDb) Save(transaction *model.Transaction) (*model.Transaction, error) {
+	err := r.Db.Save(transaction).Error
+	if err != nil {
+		return nil, err
+	}
+	return transaction, nil
+}
+
+func (r TransactionRepositoryDb) Find(id string) (*model.Transaction, error) {
+	var transaction model.Transaction
+
+	r.Db.Preload("AccountFrom.Bank").First(&transaction, "id = ?", id)
+
+	if transaction.ID == "" {
+		return nil, fmt.Errorf("no transaction was found")
+	}
+	return &transaction, nil
+}
